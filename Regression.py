a729317424677e3d1ca0cf853c494297622c1aa8
JohnPaulMills <49220579+JohnPaulMills@users.noreply.github.com>
2023-01-11 11:28:23 +0800

    Update Regression.py
    Added prediction and plotting loss and accuracy

--- Regression.py
+++ Regression.py
@@ -1,6 +1,7 @@
 # Import required libraries
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 # Create dataset (x and y into the model)
 insurance = pd.read_csv('https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv')
 # Apply normalization on data and encoding
@@ -22,11 +23,18 @@ model.compile(
     optimizer=tf.keras.optimizers.Adam(),
     metrics=['mae']
 )
-model.fit(
+history = model.fit(
     x=x_numpy,
     y=y_numpy,
     epochs=100,
 )
+# Do not use training data for prediction. Done in this instance to test the function.
+model.predict(np.expand_dims(x_numpy[0],0)) # x_numpy[0] will fail, as the shape is (11,) and not (11,1). Shape of x_numpy is (1338, 11)
+# Plot loss and accuracy
+pd.DataFrame(history.history).plot()
+# pd.DataFrame(history.history['loss']).plot() # individual plot
+# Save and load model
 model.save('saved_model/regression_model')
-print(tf.__version__)
-
+model_loaded = tf.keras.models.load_model('saved_model/regression_model')
+print(model.summary())
+print(model_loaded.summary())
