261838dfb323adf8d9dd199001f41468cf373dbc
JohnPaulMills <49220579+JohnPaulMills@users.noreply.github.com>
2023-01-11 11:39:13 +0800

    CNN for multi classification using prefetch batch dataset

---
+++ Transfer_Learning_BatchLoading.py
@@ -0,0 +1,66 @@
+# Import required libraries
+import tensorflow as tf
+import tensorflow_datasets as tfds
+# Import data
+(train_data, test_data), ds_info = tfds.load(name="food101",
+                                             split=["train", "validation"],
+                                             shuffle_files=True,
+                                             as_supervised=True, # download data in tuple format (sample, label), e.g. (image, label)
+                                             with_info=True) # include dataset metadata? if so, tfds.load() returns tuple (data, ds_info)
+# Take one sample off the training data
+train_one_sample = train_data.take(1) # samples are in format (image_tensor, label)
+# Output info about our training sample
+class_names = class_names = ds_info.features["label"].names
+for image, label in train_one_sample:
+  print(f"""
+  Image shape: {image.shape}
+  Image dtype: {image.dtype}
+  Target class from Food101 (tensor form): {label}
+  Class name (str form): {class_names[label.numpy()]}
+        """)
+# Batch and prepare datasets
+# Original dataset (e.g. train_data) -> map() -> shuffle() -> batch() -> prefetch() -> PrefetchDataset
+
+
+# Make a function for preprocessing images
+def preprocess_img(image, label, img_shape=224):
+    """
+    Converts image datatype from 'uint8' -> 'float32' and reshapes image to
+    [img_shape, img_shape, color_channels]
+    """
+    image = tf.image.resize(image, [img_shape, img_shape]) # reshape to img_shape
+    return tf.cast(image, tf.float32), label # return (float32_image, label) tuple
+
+
+# Map preprocessing function to data (and paralellize)
+train_data = train_data.map(map_func=preprocess_img, num_parallel_calls=tf.data.AUTOTUNE)
+test_data = test_data.map(preprocess_img, num_parallel_calls=tf.data.AUTOTUNE)
+# Shuffle data and turn it into batches and prefetch it (load it faster)
+train_data = train_data.shuffle(buffer_size=1000).batch(batch_size=32).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_data = test_data.batch(32).prefetch(tf.data.AUTOTUNE)
+
+
+# Create base model
+input_shape = (224, 224, 3)
+base_model = tf.keras.applications.EfficientNetB0(include_top=False)
+base_model.trainable = False # freeze base model layers
+# Create Functional model
+inputs = tf.keras.layers.Input(shape=input_shape, name="input_layer")
+# Note: EfficientNetBX models have rescaling built-in but if your model didn't you could have a layer like below
+# x = tf.keras.layers.Rescaling(1./255)(x)
+x = base_model(inputs, training=False) # set base_model to inference mode only
+x = tf.keras.layers.GlobalAveragePooling2D(name="pooling_layer")(x)
+x = tf.keras.layers.Dense(len(class_names))(x) # want one output neuron per class
+# Separate activation of output layer so we can output float32 activations
+outputs = tf.keras.layers.Activation("softmax", dtype=tf.float32, name="softmax_float32")(x)
+model = tf.keras.Model(inputs, outputs)
+model.compile(loss="sparse_categorical_crossentropy", # Use sparse_categorical_crossentropy when labels are *not* one-hot
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=["accuracy"]
+)
+model.fit(train_data,
+          epochs=3,
+          steps_per_epoch=len(train_data),
+          validation_data=test_data,
+          validation_steps=int(0.15 * len(test_data)),
+)
